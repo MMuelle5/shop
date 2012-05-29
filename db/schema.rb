a87f3909f8dc8e3c8311e5a829f082f77a0b7ef5
Marius Mueller <mmuelle5@hsz-t.ch>
2012-05-29 10:25:56 +0200

    kap 10

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120513203429) do
+ActiveRecord::Schema.define(:version => 20120529080544) do
 
   create_table "cards", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(:version => 20120513203429) do
   create_table "line_items", :force => true do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
--- test/functional/carts_controller_test.rb
+++ test/functional/carts_controller_test.rb
@@ -25,25 +25,25 @@ class CartsControllerTest < ActionController::TestCase
   end
 
   test "should show cart" do
-    get :show, id: @cart
+    get :show, id: @cart.to_param
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @cart
+    get :edit, id: @cart.to_param
     assert_response :success
   end
 
   test "should update cart" do
-    put :update, id: @cart, cart: @cart.attributes
+    put :update, id: @cart.to_param, cart: @cart.attributes
     assert_redirected_to cart_path(assigns(:cart))
   end
 
   test "should destroy cart" do
     assert_difference('Cart.count', -1) do
-      delete :destroy, id: @cart
+      session[:cart_id] = @cart.id
+      delete :destroy, id: @cart.to_param
     end
-
-    assert_redirected_to carts_path
+    assert_redirected_to store_path
   end
 end
